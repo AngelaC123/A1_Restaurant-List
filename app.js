3ad63a0e26a9ba8f3a20958ebc2d576e1802d9d5
AngelaC123 <angelarare@gmail.com>
2022-02-24 14:47:36 +0800

    Build layout:main and partial template: index Apply style on the page Built static files Used bootstrap 4.3.1, popper, font-awesome

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Set static files
+app.use(express.static('public'))
+
 // Set routes
 app.get('/', (req, res) => {
   res.render('index')
